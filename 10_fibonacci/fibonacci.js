4a5f956cd7ab1ced7747110b451d7127e8f4a048
ashxsong <beethoven6198@gmail.com>
2024-04-27 01:25:42 -0500

    Complete Exercise 10
    Completed fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  num = parseInt(num);
+  if (num === 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+  let seq = [1, 1];
+  for (let i = 0; i < num - 2; i++) {
+    seq.push(seq[i] + seq[i + 1]);
+  }
+  return seq[seq.length - 1];
 };
 
 // Do not edit below this line
