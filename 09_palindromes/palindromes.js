1a7d457100ddc2007be83608d8fdeede1f582576
ashxsong <beethoven6198@gmail.com>
2024-04-27 01:18:41 -0500

    Complete Exercise 09
    Completed palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let letters = str.toLowerCase().split("");
+  letters = letters.filter(letter => "abcdefghijklmnopqrstuvwxyz0123456789".includes(letter));
+  let originalStr = "";
+  for (let letter of letters) {
+    originalStr += letter;
+  }
+  let reverseStr = letters.reverse().join("");
+  return originalStr === reverseStr;
 };
 
 // Do not edit below this line
