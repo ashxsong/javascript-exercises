9bab5d226fd45bcf66b3a9eef85b0b3380c3b5f2
ashxsong <beethoven6198@gmail.com>
2024-04-27 01:50:48 -0500

    Complete Exercise 12
    Completed findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let arrRed = arr.reduce((obj, value) => {
+    if (value.yearOfDeath === undefined) {
+      let currentDate = new Date();
+      value.yearOfDeath = currentDate.getFullYear();
+    }
+    let age = parseInt(value.yearOfDeath) - parseInt(value.yearOfBirth);
+    obj[age] = value;
+    return obj;
+  }, {});
+  let keys = Object.keys(arrRed);
+  keys.sort((a, b) => b - a);
+  return arrRed[keys[0]];
 };
 
 // Do not edit below this line
