1c0ed61ad977089d08a8c0151ee092433d8852ad
ashxsong <beethoven6198@gmail.com>
2024-04-27 00:54:04 -0500

    Complete Exercise 08
    Completed add, subtract, sum, multiply, power, and factorial functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, num) => prod *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let prod = 1;
+  for (i = 1; i < num + 1; i++) {
+    prod *= i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
